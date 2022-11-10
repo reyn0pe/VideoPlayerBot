cade8dc2b5f2875752e58128cd1f719fd17c7584
thesanzu <116525838+thesanzu@users.noreply.github.com>
2022-11-11 03:39:19 +0700

    Add files via upload

---
+++ translations.py
@@ -0,0 +1,22 @@
+from config import ASSISTANT_NAME
+from helpers.bot_utils import BOT_NAME, USERNAME
+
+
+START_TEXT = f" **ᴏɪ ᴋᴏɴᴛᴏʟ**, \n\nᴋᴇɴᴀʟɪɴ ɢᴜᴇ **{BOT_NAME}** \nɢᴜᴇ ʙɪsᴀ ʙᴀɴᴛᴜ ʟᴏ ʙᴜᴀᴛ ᴘʟᴀʏ ᴠɪᴅᴇᴏ ᴅɪ ᴏʙʀᴏʟᴀɴ sᴜᴀʀᴀ, ʙɪᴀsᴀɴʏᴀ sɪʜ ᴅɪᴘᴀᴋᴇ ᴀsᴜᴘᴀɴ ᴠɪᴅᴇᴏ ʙᴏᴋᴇᴘ sᴀᴍᴀ ᴀɴᴀᴋ ᴋᴏɴᴛᴏʟ.\n\n**ᴘᴇɴᴄɪᴘᴛᴀ ɢᴜᴇ sɪ ʙᴏᴅ @sanzetc!** "
+HELP_TEXT = f"""
+ » **sᴇᴛᴛɪɴɢ ᴜᴘ ʙᴏᴛ** «
+
+\u2022 ᴍᴜʟᴀɪ ᴏs ᴅɪ ɢᴄ ʟᴏ!
+\u2022 ᴛᴀᴍʙᴀʜɪɴ ɢᴜᴇ (@{USERNAME}) & ʙᴀʙᴜ ɢᴜᴇ (@{ASSISTANT_NAME}) ᴅɪ ɢʀᴜᴘ ʟᴏ!
+\u2022 ɢᴜᴇ ɴʏᴀ ᴊᴀᴅɪɪɴ ᴀᴅᴍɪɴ ᴅᴜʟᴜ ʟᴀʜ ᴋᴏɴᴛᴏʟ (@{USERNAME}) & ʙᴀʙᴜ ɢᴜᴇ (@{ASSISTANT_NAME}) ᴅɪ ɢʀᴜᴘ ʟᴏ!
+
+» **ᴀᴠᴀɪʟᴀʙʟᴇ ᴄᴏᴍᴍᴀɴᴅs** «
+
+\u2022 `/play` - sᴛʀᴇᴀᴍɪɴɢ ᴀᴜᴅɪᴏ
+\u2022 `/stream` - sᴛʀᴇᴀᴍɪɴɢ ᴠɪᴅᴇᴏ
+\u2022 `/pause` - ᴊᴇᴅᴀ sᴛʀᴇᴀᴍɪɴɢ
+\u2022 `/resume` - ʟᴀɴᴊᴜᴛ sᴛʀᴇᴀᴍɪɴɢ ʏᴀɴɢ ᴅɪ ᴊᴇᴅᴀ
+\u2022 `/endstream` - sᴛᴏᴘ sᴛʀᴇᴀᴍɪɴɢ & ᴘᴇʀɢɪ ᴅᴀʀɪ ʜᴀᴅᴀᴘᴀɴ ʟᴏ ʏᴀɴɢ ᴋᴀʏᴀᴋ ᴋᴏɴᴛᴏʟ
+\u2022 `/restart` - ʀᴇsᴛᴀʀᴛ ʙᴏᴛ (sᴜᴅᴏ ᴏɴʟʏ)
+"""
+ABOUT_TEXT = f"» **ɪɴғᴏʀᴍᴀᴛɪᴏɴ** «\n\nThis bot is created for streaming videos in telegram group video chats using several methods from WebRTC. Powered by pytgcalls the async client API for the Telegram Group Calls and Pyrogram the telegram MTProto API Client Library and Framework in Pure Python for Users and Bots. \n\n**This bot licensed under GNU-GPL 3.0 License!**"
