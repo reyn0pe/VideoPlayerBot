0443f05c71442ff60c505eda0d9c75e502f8cc82
thesanzu <116525838+thesanzu@users.noreply.github.com>
2022-11-11 04:38:09 +0700

    Create nopm.py

---
+++ plugins/nopm.py
@@ -0,0 +1,38 @@
+"""
+© thesanzu
+"""
+
+import asyncio
+from pyrogram import Client, filters
+from helpers.bot_utils import USERNAME
+from pyrogram.errors import BotInlineDisabled
+from config import API_ID, API_HASH, SESSION_STRING, REPLY_MESSAGE, OLD_PMS
+
+User = Client(
+    SESSION_STRING,
+    API_ID,
+    API_HASH
+)
+
+
+@User.on_message(filters.private & filters.incoming & ~filters.bot & ~filters.service & ~filters.me & ~filters.edited & ~filters.chat([777000, 454000]))
+async def nopm(client, message):
+    if REPLY_MESSAGE is not None:
+        try:
+            inline = await client.get_inline_bot_results(USERNAME, "SAF_ONE")
+            m = await client.send_inline_bot_result(
+                message.chat.id,
+                query_id=inline.query_id,
+                result_id=inline.results[0].id,
+                hide_via=True
+                )
+            old = OLD_PMS.get(message.chat.id)
+            if old:
+                await client.delete_messages(message.chat.id, [old["msg"], old["s"]])
+            OLD_PMS[message.chat.id] = {"msg":m.updates[1].message.id, "s":message.message_id}
+        except BotInlineDisabled:
+            print(f"[𝗪𝗔𝗥𝗡] - ɪɴʟɪɴᴇ sɪ ᴋᴏɴᴛᴏʟ @{USERNAME} ʙᴇʟᴜᴍ ᴅɪ ᴀᴋᴛɪғɪɴ. ᴀᴋᴛɪғɪɴ ᴅᴜʟᴜ ᴅɪ @Botfather ʙᴜᴀᴛ ᴀᴋᴛɪғɪɴ ᴘᴍ ᴘᴇʀᴍɪᴛ !")
+            await message.reply_text(f"{REPLY_MESSAGE}\n\n<b>© ᴘᴏᴡᴇʀᴇᴅ ʙʏ : \n@sanzetc | @reyn0pe </b>")
+        except Exception as e:
+            print(e)
+            pass
